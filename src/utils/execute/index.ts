fd23e562dc11bbecab9f81dfef63285477cd63a7
Andrew <john@existtribe.com>
2022-04-07 03:59:48 +0000

    Modified async exec

--- src/utils/execute/index.ts
+++ src/utils/execute/index.ts
@@ -1,10 +1,20 @@
 import { exec, execFile } from "child_process";
 import { promisify } from "util";
 
-export async function execute(command: string) {
-    const execute_command = promisify(exec)
+export async function execute(command: string): Promise<{ stdout: any, stderr: string }> {
+    // const execute_command = promisify(exec)
 
-    return execute_command(command)
+    // return execute_command(command)
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if(error)
+                return reject(error)
+
+            resolve({
+                stdout, stderr
+            })
+        })
+    })
 }
 
 export async function executeFile(filePath: string) {
